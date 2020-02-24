1a2826313325706ed2fd38814d41ed7b2b8379bd
Zeros遇见 <zeros_zhang@163.com>
2020-02-24 17:17:54 +0800

    修改了一点描述，不然太丑了

--- 01.每次执行这个程序，用于更新README.py
+++ 01.每次执行这个程序，用于更新README.py
@@ -3,7 +3,7 @@ import os
 # 文件头
 file_head = """# Python-Study
 该库主要用python的语法学习与复习
-记得在每次更新库之后，执行'01.每次执行这个程序，用于更新README.md.py'用于更新该文件的目录。
+记得在每次更新库之后，执行'01.每次执行这个程序，用于更新README.py'用于更新该文件的目录。
 
 ## 该库的目录如下(便于后期查找)：
 """
@@ -23,7 +23,7 @@ for file in os.listdir(Folder_Path):
 with open(README_path, 'w', encoding='utf-8') as ReadMe_F:
     print(file_head, file=ReadMe_F)
     for x in python_file:
-        print(x, '  ', file=ReadMe_F)
+        print(x, '  ', file=ReadMe_F)           # 加俩空格，md文件才能显示换行
 file_num = len(python_file)
 print()
 print(f'以上文件均已写入README.md文件中，共{file_num}个！')
