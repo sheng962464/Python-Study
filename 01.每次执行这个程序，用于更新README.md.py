d8e2593e0987fa1a1cd53a7f8f76b6ee508c48c3
Zeros遇见 <zeros_zhang@163.com>
2020-02-24 17:10:44 +0800

    更新README.md文件，并提供程序更新

---
+++ 01.每次执行这个程序，用于更新README.md.py
@@ -0,0 +1,29 @@
+import os
+
+# 文件头
+file_head = """# Python-Study
+该库主要用python的语法学习与复习
+记得在每次更新库之后，执行'01.每次执行这个程序，用于更新README.md.py'用于更新该文件的目录。
+
+## 该库的目录如下(便于后期查找)：
+"""
+README_path = 'README.md'
+Folder_Path = os.path.dirname(os.path.abspath(__file__))
+print('当前目录的绝对路径为：', Folder_Path)
+print()
+print('当前目录下的如下：')
+
+python_file = []
+
+for file in os.listdir(Folder_Path):
+    if os.path.splitext(file)[1] == '.py':
+        python_file.append(file)
+        print(file)
+
+with open(README_path, 'w', encoding='utf-8') as ReadMe_F:
+    print(file_head, file=ReadMe_F)
+    for x in python_file:
+        print(x, file=ReadMe_F)
+file_num = len(python_file)
+print()
+print(f'以上文件均已写入README.md文件中，共{file_num}个！')
---
+++ README.md
@@ -0,0 +1,16 @@
+# Python-Study
+该库主要用python的语法学习与复习
+记得在每次更新库之后，执行'01.每次执行这个程序，用于更新README.md.py'用于更新该文件的目录。
+
+## 该库的目录如下(便于后期查找)：
+
+01.每次执行这个程序，用于更新README.md.py
+Json包测试.py
+Pickle包测试.py
+print(f)测试.py
+Strip方法测试.py
+super()函数测试-2.py
+super()函数测试.py
+Time包测试(包含Json与Pickle包对比).py
+__new__方法详解及使用.py
+列表合并方法.py
