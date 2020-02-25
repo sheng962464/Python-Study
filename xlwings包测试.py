161030e834c0cf6e9915ad3e12887476c70aa30c
Zeros遇见 <zeros_zhang@163.com>
2020-02-25 17:14:08 +0800

    更新学习进度 xlwings包测试.py python常用小技巧.py

--- xlwings包测试.py
+++ xlwings包测试.py
@@ -11,16 +11,19 @@ try:
     info = ws.used_range
     sheet0_row = info.last_cell.row
     sheet0_column = info.last_cell.column
-    print(f'共{sheet0_row}行,{sheet0_column}列')
+
     # 把表单中的内容转化为列表(去掉表头)
     Sheet0_List = ws.range((2, 1), (sheet0_row, sheet0_column)).value
     Sheet0_Head = ws.range((1, 1), (1, 2)).value
-    print(Sheet0_Head)
     with open(error_code_text_path, 'w', encoding='utf-8') as f:
+        print('# Python异常代码含义对照表', file=f)
+        # 打印表头
         print(f'|{Sheet0_Head[0]}|{Sheet0_Head[1]}|', file=f)
         print('|:----|:----|', file=f)
+        # 打印内容
         for x in Sheet0_List:
             print(f'|{x[0]}|   {x[1]}|', file=f)
 
+    print(f'共{sheet0_row}行,{sheet0_column}列,输入结束')
 finally:
     app.quit()
