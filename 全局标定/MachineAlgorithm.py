d52251e133398b74b1cab7b92999d9fe3900b390
Zeros遇见 <zeros_zhang@163.com>
2020-03-19 16:28:35 +0800

    全局标定算法仿真程序

--- 全局标定/MachineAlgorithm.py
+++ 全局标定/MachineAlgorithm.py
@@ -4,20 +4,28 @@ import BaseTransfer
 
 
 # 传感器扫描函数
-def sensor_scan(x_sensor, x_travel, x_object):
+def sensor_scan(x_sensor, x_plane):
     """
     使用的传感器，传感器运动的轨迹，扫描的物体
     """
+    result = []
+    for x_line in x_sensor.laser:
+        result.append(BaseAlgorithm.intersection_of_line_and_plane(x_line, x_plane))
+    return result
 
 
 def test_unit():
+    result = []
     m_sensor = BaseClass.sensor(x_location=(0, 0, 1))
-    m_sensor.sensor_init()
-    path = [[0, i * 0.1, 10] for i in range(-10, 10)]
+    path = [[0, i * 0.01, 10] for i in range(-100, 100)]
     for xpath in path:
         m_sensor.sensor_absolute_move(xpath)
-        for x in m_sensor.trigPoint:
-            xline = BaseClass.line(x, m_sensor.__laser_vector)
+        result.append(sensor_scan(m_sensor, BaseClass.plane()))
+    print('扫描结束')
+    with open(r'D:\全局标定测试\result.txt', 'w') as f:
+        for x in result:
+            for y in x:
+                print(f'{y.x},{y.y},{y.z}', file=f)
 
 
 if __name__ == '__main__':
